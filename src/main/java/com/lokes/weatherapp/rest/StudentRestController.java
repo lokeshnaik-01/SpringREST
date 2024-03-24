4765cb1aee9314df8643c9ff00eaec98e3c96362
lokeshnaik-01 <lavadiyalokeshnaik@gmail.com>
2024-03-24 19:15:57 +0530

    capture all exceptions

--- src/main/java/com/lokes/weatherapp/rest/StudentRestController.java
+++ src/main/java/com/lokes/weatherapp/rest/StudentRestController.java
@@ -49,4 +49,17 @@ public class StudentRestController {
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    // catch all exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
